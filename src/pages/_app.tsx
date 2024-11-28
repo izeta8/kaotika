9b391c97ec4836cc9fe1b4c6551b290ada4fcb9d
ander <ander.zubizarreta@ikasle.aeg.eus>
2024-11-28 09:49:00 +0100

    [MENDI-485]hardcode to enter in kaotika

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,26 @@ import { AppProps } from 'next/app';
 import "../styles/globals.css";
 import {NextUIProvider} from "@nextui-org/react";
 import Head from "next/head";
+import { useState, useEffect } from "react";
 
 
 function MyApp({ Component, pageProps }: AppProps) {
 
+  const mockSession = {
+    user: {
+      name: "Ander Zubizarreta",
+      email: "ander.zubizarreta@ikasle.aeg.eus",
+      image: "",
+    },
+    expires: "2025-12-31T23:59:59.999Z",
+    accessToken: "fake-access-token",
+    refreshToken: "fake-refresh-token",
+    email: "jon.pazos@ikasle.aeg.eus",
+  };
+
+
   return (
-    <SessionProvider session={pageProps.session}>
+    <SessionProvider session={mockSession}>
       <NextUIProvider>
           <Head>
             <link rel='icon' href='/favicon.ico' />
