21a30f0284263fa2ecb81b48a59ef8d0e410afdf
ander <ander.zubizarreta@ikasle.aeg.eus>
2024-11-28 13:19:53 +0100

    [MENDI-484]Creation of home shop epic designe

--- src/pages/shop/shopHome.tsx
+++ src/pages/shop/shopHome.tsx
@@ -1,40 +1,106 @@
 import Layout from "@/components/Layout";
 import Link from 'next/link';
+import Image from "next/image";
+import { useEffect } from "react";
 
 const ShopHome = () => {
-  console.log("home page");
+
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    return () => {
+      document.body.style.overflow = '';
+    };
+  }, []);
 
   return (
     <Layout>
-        <h1 className="text-5xl font-bold mb-8">Welcome to Shop</h1>
-        <div className="flex space-x-4">
-          {/* Button 1: Equipment */}
-          <Link
-            href="/shop/buy/equipment"
-            className="px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition"
-          >
-            Equipment
-          </Link>
-
-          {/* Button 2: Magical Stuff */}
-          <Link
-            href="/shop/buy/magical-stuff"
-            className="px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition"
-          >
-            Magical Stuff
-          </Link>
-
-          {/* Button 3: Sell */}
-          <Link
-            href="/shop/sell"
-            className="px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition"
-          >
-            Sell
-          </Link>
-        </div>
+      <Background />
+       <div className="relative z-30 flex flex-row items-start justify-center h-screen bg-transparent pt-20 space-x-10 sm:space-x-20">
+        <EquipmentShop />  
+        <SellItem />
+        <MagicalStuffShop />
+      </div>
     </Layout>
   );
 };
 
+const EquipmentShop = () => {
+  return (
+    <Link
+      href="/shop/buy/equipment"
+      className="flex items-center justify-center text-white rounded-lg transition transform hover:scale-105 px-4 py-2"
+    >
+      <Image
+        src="/images/shop/Equipment_buy_button.png" 
+        alt="Equipment"
+        width={309}
+        height={100}
+        className="mr-2"
+        draggable={false}
+      />
+    </Link>
+  );
+}
+
+const MagicalStuffShop = () => {
+  return (
+    <Link
+      href="/shop/buy/magical-stuff"
+      className="flex items-center justify-center text-white rounded-lg transition transform hover:scale-105 px-4 py-2"
+    >
+      <Image
+        src="/images/shop/Magical_stuff_button.png" 
+        alt="Magical Stuff"
+        width={300}
+        height={100}
+        className="mr-2"
+        draggable={false}
+      />
+    </Link>
+  );
+}
+
+const SellItem = () => {
+  return (
+    <Link
+      href="/shop/sell"
+      className="flex items-center justify-center text-white rounded-lg transition transform hover:scale-105 px-4 py-2"
+    >
+      <Image
+        src="/images/shop/Sell_item_button.png" 
+        alt="Sell item"
+        width={282}
+        height={100}
+        className="mr-2"
+        draggable={false}
+      />
+    </Link>
+  );
+}
+
+const Background = () => {
+  return (
+    <>
+      <div
+        className='w-full h-full absolute top-0 opacity-20 z-10'
+        style={{
+          backgroundColor: '#191A1D',
+          backgroundImage: "url('/images/shop/background_pattern.png')",
+          backgroundRepeat: "repeat", 
+          backgroundPosition: "center", 
+          backgroundSize: '8%',
+        }}
+      />
+      <div
+        className='w-full h-full absolute top-0 z-0'
+        style={{
+          backgroundColor: '#191A1D',
+        }}
+      />
+    </>
+  );
+}
+
 export default ShopHome;
+
 
