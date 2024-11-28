fe9fe698e7abece56f0c7dc5d32dd2dd478b31b0
izeta8 <julen.izeta@ikasle.aeg.eus>
2024-11-28 09:00:57 +0100

    [MENDI-504][NEW] Create shop and sell pages (empty)

---
+++ src/pages/shop/index.tsx
@@ -0,0 +1,10 @@
+
+const ShopHome = () => {
+  return (
+    <>
+      <h1>SELECT SELL / BUY</h1>
+    </>
+  );
+};
+
+export default ShopHome;
---
+++ src/pages/shop/sell.tsx
@@ -0,0 +1,10 @@
+
+const Sell = () => {
+  return (
+    <>
+      <h1>SELL</h1>
+    </>
+  );
+};
+
+export default Sell;
