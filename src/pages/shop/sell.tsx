e90fc503f36ce3c489870cc6d89897fc98da665e
mikel161211231 <mikel.lopez@ikasle.aeg.eus>
2024-11-28 12:23:19 +0100

    [MENDI-493][UPDATE] Add the background to the sell shop screen

--- src/pages/shop/sell.tsx
+++ src/pages/shop/sell.tsx
@@ -1,18 +1,26 @@
 import Layout from "@/components/Layout";
 import Link from 'next/link';
+
 const Sell = () => {
   return (
+  
     <Layout>
-   <h1 className="text-5xl font-bold mb-8">SELL</h1>
+      <div className="flex flex-col text-medievalSepia bg-cover bg-no-repeat bg-center h-full" style={{ backgroundImage: 'url(/images/shop/background_sell_shop.jpg)'}}>
+     
+       <h1 className="text-5xl font-bold mb-8">SELL</h1>
         {/* Botón de Return */}
         <Link
           href="/shop/shopHome"
-          className="mt-4 px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition"
+          className="mt-4 px-6 py-3 text-center text-3xl text-black rounded-lg hover:bg-blue-600 transition max-w-36	"
         >
           Return to Home
         </Link>
+        </div>
   </Layout>
+  
+  
   );
 };
+
 
 export default Sell;
