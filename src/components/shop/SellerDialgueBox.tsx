074b63dadbe02108a6ac8300f7681f217966be44
mikel161211231 <mikel.lopez@ikasle.aeg.eus>
2024-12-02 22:01:09 +0100

    [MENDI-493][NEW] Create the Seller dialoge box and add it to the sell shop screen

---
+++ src/components/shop/SellerDialgueBox.tsx
@@ -0,0 +1,23 @@
+
+
+interface promps {
+    phrase?: string,
+}
+
+
+const SellerDialogueBox : React.FC<promps> = ({phrase}) => {
+
+    return (
+
+
+<div className="flex flex-row text-medievalSepia bg-cover bg-no-repeat bg-center w-2 mt-80 mr-10" style={{ backgroundImage: 'url(/images/shop/seller_dialogue_box.png)', width: '525px', height: '280px' }}>
+    <h2 className="text-3xl pt-10 pl-8 mb-4 text-center">{phrase}</h2>
+</div>
+             
+        
+    );
+
+}
+
+
+export default SellerDialogueBox;
--- src/pages/shop/sell.tsx
+++ src/pages/shop/sell.tsx
@@ -4,6 +4,9 @@ import PlayerInventorySellShop from "@/components/shop/PlayerInventorySellShop";
 import Link from 'next/link';
 import player from "./FakeTestPlayer";
 import SellShopObjectDetails from "@/components/shop/SellShopObjectDetails";
+import SellerDialogueBox from "@/components/shop/SellerDialgueBox";
+import { Button } from "@nextui-org/button";
+import KaotikaButton from "@/components/KaotikaButton";
 
 
 const Sell = () => {
@@ -19,15 +22,35 @@ const Sell = () => {
           <SellShopObjectDetails />
         </div>
 
-        <div className="flex flex-row w-1/2">
+        <div className="flex flex-col w-1/2">
 
-          {/* Botón de Return */}
-          <Link
-            href="/shop/shopHome"
-            className="mt-4 px-6 py-3 text-center text-3xl text-black rounded-lg hover:bg-blue-600 transition max-w-36 max-h-24	"
-          >
-            Return to Home
-          </Link>
+          <div className="flex flex-row">
+
+
+            <div className="mt-4 px-6">
+              <KaotikaButton text="KEEP IT" />
+            </div>
+            <div className="mt-4 px-6">
+              <KaotikaButton text="SELL IT" />
+            </div>
+
+            {/* Botón de Return */}
+            <Link
+              href="/shop/shopHome"
+              className="mt-4 px-6 py-3 text-center text-3xl text-black rounded-lg hover:bg-blue-600 transition max-w-36 max-h-24"
+            >
+              Return to Home
+            </Link>
+
+          </div>
+
+          <div className="flex flex-row  place-content-end">
+            <SellerDialogueBox phrase="Welcome to the best trade shop of the continent!!!" />
+          </div>
+
+
+
+
         </div>
 
       </div>
