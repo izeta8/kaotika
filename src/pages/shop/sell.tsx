9945fac5bdfe0a98ca29d7a12406826de9aa4925
mikel161211231 <mikel.lopez@ikasle.aeg.eus>
2024-12-03 22:24:26 +0100

    [MENDI-495][UPDATE] Change the fake inventory data with the db data of the player

--- src/pages/shop/sell.tsx
+++ src/pages/shop/sell.tsx
@@ -8,9 +8,76 @@ import SellerDialogueBox from "@/components/shop/SellerDialgueBox";
 import { Button } from "@nextui-org/button";
 import KaotikaButton from "@/components/KaotikaButton";
 import ShopPlayerInfo from "@/components/shop/ShopPlayerInfo";
+import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+import Loading from "@/components/Loading";
 
 
 const Sell = () => {
+
+
+  const [loading, setLoading] = useState(true);
+  const { data: session } = useSession();
+  const [playerEmail, setPlayerEmail] = useState<string | null>(null);
+  const [playerData, setPlayerData] = useState<Player | null>(null);
+
+
+
+//####################################################################################################
+//####################################################################################################
+// Get the player data with the email that the user has been autenticated
+//####################################################################################################
+//####################################################################################################
+
+  useEffect(() => {
+    if (session?.user?.email) {
+      setLoading(true);
+      setPlayerEmail(session.user.email);
+    }
+  }, [session]);
+
+  useEffect(() => {
+    if (playerEmail) {
+
+      const loadPlayerData = async () => {
+        try {
+          setLoading(true);
+          const res = await fetch(`/api/shop/player/?playerEmail=${playerEmail}`);
+          if (res.status === 200) {
+            const data = await res.json();
+            delete data.player.isInsideTower;
+            delete data.player.socketId;
+            delete data.player.isInsideLab;
+            delete data.player.isInsideHall;
+            delete data.player.obituaryDiscovered;
+            delete data.player.fcm_token;
+            delete data.player.role;
+            delete data.player.cardId;
+            const playerObject = data.player;
+
+            console.log(playerObject)
+            setPlayerData(playerObject);
+          } else if (res.status === 404) {
+            const data = await res.json();
+          }
+        } catch (error) {
+          console.error(`An error occurred while checking registration`, error);
+        } finally {
+          setLoading(false);
+        }
+      }
+
+      loadPlayerData();
+    }
+  }, [playerEmail]);
+
+//####################################################################################################
+// Return loading spinner if there is charging something
+//####################################################################################################
+
+  if (loading) {
+    return (<Loading />);
+  }
 
 
   return (
@@ -19,7 +86,7 @@ const Sell = () => {
       <div className="flex text-medievalSepia bg-cover bg-no-repeat bg-center min-h-screen" style={{ backgroundImage: 'url(/images/shop/background_sell_shop.jpg)' }}>
 
         <div className="flex-col w-1/2">
-          <PlayerInventorySellShop playerData={player} />
+          <PlayerInventorySellShop playerData={playerData} />
           <SellShopObjectDetails />
         </div>
 
