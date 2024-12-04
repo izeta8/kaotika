2551f5db310710a1fc02e9adf612fe2bf294bc3f
izeta8 <julen.izeta@ikasle.aeg.eus>
2024-12-04 09:16:13 +0100

    [MENDI-482][UPDATE] Start implementing item modal. (Not finished yet)

--- src/pages/shop/buy/[shopType].tsx
+++ src/pages/shop/buy/[shopType].tsx
@@ -87,8 +87,11 @@ const Shop = () => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [ingredietsInCart,setIngredientsInCart] = useState([]);
   const [equipmentInCart,setEquipmentInCart] = useState([]);
-  const [categoryData, setCategoryData] = useState<Array<ItemData>>([]);
 
+  const [itemModalShown, setItemModalShown] = useState(true);
+  const [modalItemData, setModalItemData] = useState<ItemData|undefined>();
+
+  const [categoryData, setCategoryData] = useState<Array<ItemData>>([]);
   const [currentCategory, setCurrentCategory] = useState<string>('');
 
   // ---- SHOP ITEMS ----  //
@@ -164,6 +167,13 @@ const Shop = () => {
     currentShopType.forEach(categoryObject => loadLocalStorageIntoStates(categoryObject));
   }, []);
 
+  useEffect(() => {
+    if (itemModalShown) {
+      document.body.classList.add("overflow-hidden");
+    } else {
+      document.body.classList.remove("overflow-hidden");
+    }
+  }, [itemModalShown]);
 
   // Handle cart
   useEffect(() => {
@@ -219,32 +229,63 @@ const Shop = () => {
     <div
       className="relative min-h-screen flex flex-col bg-[#191A1D] bg-repeat-center"
     >
+
       <Layout>
-        <ShopHeader currentCategory={currentCategory} setCurrentCategory={setCurrentCategory} onCartClick={openCart}/>
-        <ShopPlayerInfo />
-        <ShopContent currentCategory={currentCategory} categoryData={categoryData}/>  
-        <Background />
 
-        {isCartOpen && (
-          <div className="fixed inset-0 z-50">
-            <div
-              className="absolute inset-0 bg-black/60 pointer-events-auto"
-              onClick={closeCart} // Close modal when clicking outside
-            ></div>
+        <div className="relative -mt-2">
 
-            {/* Cart Modal */}
-            <Cart
-              isOpen={isCartOpen}
-              onClose={closeCart}
-              ingredients={fakeIngredients}
-              equipment={fakeEquipment}
+          {itemModalShown && (
+            <ItemModal 
+            itemModalShown={itemModalShown}
+            setItemModalShown={setItemModalShown}
+            itemData={modalItemData}
             />
-          </div>
-        )}
+          )}
+
+          <ShopHeader 
+            currentCategory={currentCategory}
+            setCurrentCategory={setCurrentCategory}
+            onCartClick={openCart}
+          />
+
+          <ShopPlayerInfo />
+
+          <ShopContent 
+            categoryData={categoryData}
+            setItemModalShown={setItemModalShown}
+            setModalItemData={setModalItemData}
+          />  
+
+          <Background />
+
+          {isCartOpen && (
+            <div className="fixed inset-0 z-40">
+              <div
+                className="absolute inset-0 bg-black/60 pointer-events-auto"
+                onClick={closeCart} // Close modal when clicking outside
+              ></div>
+
+              {/* Cart Modal */}
+              <Cart
+                isOpen={isCartOpen}
+                onClose={closeCart}
+                ingredients={fakeIngredients}
+                equipment={fakeEquipment}
+              />
+            </div>
+          )}
+        </div>
+
       </Layout> 
     </div>
   );
 };
+
+
+// --------------------------- //
+// -----   SHOP HEADER   ----- //
+// --------------------------- //
+
 
 const ShopHeader:React.FC<{onCartClick: Function, currentCategory: string, setCurrentCategory: Function}> = ({onCartClick, currentCategory, setCurrentCategory}) => {
  
@@ -277,7 +318,7 @@ const ShopHeader:React.FC<{onCartClick: Function, currentCategory: string, setCu
         {/* Cart Button Section */}
         <div className="flex items-center">
            <button 
-            onClick={onCartClick} 
+            onClick={() => onCartClick()} 
             className="relative bg-amber-500 hover:bg-amber-600 text-black  px-8  py-6  rounded-full transition transform hover:scale-105 focus:outline-none"
           >
             <FaShoppingCart size={27} className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
@@ -290,7 +331,9 @@ const ShopHeader:React.FC<{onCartClick: Function, currentCategory: string, setCu
 
     </header>
   );
-}
+} 
+
+// -----  LINKS (CATEGORIES)  ----- //
 
 const HeaderLink: React.FC<{category: string, currentCategory: string, setCurrentCategory: Function}> = ({category, currentCategory, setCurrentCategory}) => {
   
@@ -309,19 +352,31 @@ const HeaderLink: React.FC<{category: string, currentCategory: string, setCurren
   )
 }
 
-const ShopContent: React.FC<{categoryData: Array<ItemData>, currentCategory: string}> = ({categoryData, currentCategory}) => {
+
+// ---------------------------- //
+// -----   SHOP CONTENT   ----- //
+// ---------------------------- //
+
+
+const ShopContent: React.FC<{categoryData: Array<ItemData>, setItemModalShown: Function, setModalItemData: Function}> = ({categoryData, setItemModalShown, setModalItemData}) => {
 
   return (
     <section className='w-full h-full relative z-30 flex justify-center items-center'>    
   
       {/* FILTER AND SORT BY */}
-      <ItemsList currentCategory={currentCategory} categoryData = {categoryData}/>
+      <ItemsList categoryData = {categoryData} setItemModalShown={setItemModalShown} setModalItemData={setModalItemData} />
 
     </section>
   );
 }
 
-const ItemsList: React.FC<{categoryData: Array<ItemData>, currentCategory: string}> = ({categoryData, currentCategory}) => {
+
+// -------------------------- //
+// -----   ITEMS GRID   ----- //
+// -------------------------- //
+
+
+const ItemsList: React.FC<{categoryData: Array<ItemData>, setItemModalShown: Function, setModalItemData: Function}> = ({categoryData, setItemModalShown, setModalItemData}) => {
 
   if (categoryData.length === 0) {
     return <h2 className="text-4xl m-10 text-medievalSepia">There are no available items in this category</h2>;
@@ -330,14 +385,20 @@ const ItemsList: React.FC<{categoryData: Array<ItemData>, currentCategory: strin
   return (
     <div className="w-11/12 my-10 grid grid-cols-5 gap-8 place-items-center"> 
       {categoryData.map((item: ItemData, index: number) => {
-         return <Card key={index} itemData={item} currentCategory={currentCategory} />
+         return <Card key={index} itemData={item} setItemModalShown={setItemModalShown} setModalItemData={setModalItemData} />
         })
       }
     </div>
   )
 }
 
-const Card: React.FC<{itemData: ItemData, currentCategory: string}> = ({itemData, currentCategory}) => {
+
+// ------------------------- //
+// -----   ITEM CARD   ----- //
+// ------------------------- //
+
+
+const Card: React.FC<{itemData: ItemData, setItemModalShown: Function, setModalItemData: Function }> = ({itemData, setItemModalShown, setModalItemData}) => {
 
   const router = useRouter();
 
@@ -362,10 +423,24 @@ const Card: React.FC<{itemData: ItemData, currentCategory: string}> = ({itemData
 
   const equipmentTextGradient = "bg-gradient-to-b from-[#FFD0A0] via-[#EED1B4] to-[#B2AF9E]"; 
   const magicalStuffTextGradient = "bg-gradient-to-b from-[#212532] via-[#9CB5EA] to-[#3A3C45]"; 
+ 
+  // ---- UTILITY ---- //
 
+  const handleCartClick = () => {
+    setModalItemData(itemData);
+    setItemModalShown(true);
+  } 
+  
+  const handleBuy = () => {
+    console.log("HANDLE BUY");
+  }  
+
+  const handleAddCart = () => {
+    console.log("HANDLE ADD TO CART");
+  } 
                                                   
   return (
-    <div className="bg-slate-900 w-72 p-6 flex flex-col justify-center items-center relative z-10 select-none" 
+    <div className="bg-slate-900 w-72 p-6 flex flex-col justify-center items-center relative z-10 select-none hover:cursor-pointer hover:-translate-y-4 transition-all" 
       style={{
         height: 420,
         backgroundImage: backgroundPath,
@@ -373,6 +448,7 @@ const Card: React.FC<{itemData: ItemData, currentCategory: string}> = ({itemData
         WebkitBackgroundSize: 'contain',
         backgroundSize: '100%'
       }}
+      onClick={() => handleCartClick()}
     > 
        
       <div className="flex flex-col justify-center items-center gap-3 z-30"> 
@@ -411,8 +487,8 @@ const Card: React.FC<{itemData: ItemData, currentCategory: string}> = ({itemData
 
         {/* BUY BUTTONS */}
         <div className="w-full flex flex-row gap-4">
-          <CardButton onClick={() => {console.log("HANDLE BUY")}} label="BUY"/> 
-          <CardButton onClick={() => {console.log("HANDLE ADD TO CART")}} label="ADD TO CART"/> 
+          <CardButton onClick={(e) => {e.stopPropagation(); handleBuy()}} label="BUY"/> 
+          <CardButton onClick={(e) => {e.stopPropagation(); handleAddCart()}} label="ADD TO CART"/> 
         </div>
 
       </div>
@@ -424,9 +500,11 @@ const Card: React.FC<{itemData: ItemData, currentCategory: string}> = ({itemData
       />
     </div>
   );
-} 
+}  
 
-const CardButton: React.FC<{onClick: Function, label: string}> = ({onClick, label}) => {
+// -----  CARD BUTTON  ----- //
+
+const CardButton: React.FC<{ onClick: React.MouseEventHandler<HTMLButtonElement>, label: string}> = ({onClick, label}) => {
 
   if (!label || !onClick) {return}
 
@@ -445,6 +523,8 @@ const CardButton: React.FC<{onClick: Function, label: string}> = ({onClick, labe
 
 }
  
+// -----  GOLD AND MIN XP CONTANIERS  ----- //
+
 const ItemDataLabel: React.FC<{image: string, data: number, title: string}> = ({ image, data, title }) => {
 
   if (!data) return null;
@@ -473,6 +553,130 @@ const ItemDataLabel: React.FC<{image: string, data: number, title: string}> = ({
 };
 
 
+// -------------------------- //
+// -----   ITEM MODAL   ----- //
+// -------------------------- //
+
+   
+const ItemModal: React.FC<{itemModalShown: boolean, setItemModalShown: Function, itemData: ItemData | undefined}> = ({itemModalShown, setItemModalShown, itemData}) => {
+   
+  if (!itemData) {return}
+  const router = useRouter();
+
+  // const [isVisible, setIsVisible] = useState(itemModalShown);
+
+  // useEffect(() => {
+  //   if (itemModalShown) {
+  //     setIsVisible(true);
+  //   } else {
+  //     // Espera a que la transición termine antes de desmontar
+  //     const timer = setTimeout(() => setIsVisible(false), 300); // 300ms coincide con la duración de la transición
+  //     return () => clearTimeout(timer);
+  //   }
+  // }, [itemModalShown]);
+
+  // console.log("itemModalShown");
+  // console.log(itemModalShown);
+
+
+  const {_id, name, description, type, value, modifiers, min_lvl, image, base_percentage, defense} = itemData;
+
+  const image_url = `https://kaotika.vercel.app${image}`; 
+
+  const modalBackgroundImage = isMagicalStuffShop(router) ? "url('/images/shop/buy/magical_stuff_modal_background.png')" : "url('/images/shop/buy/equipment_modal_background.png')";
+ 
+  // Style variables 
+  const equipmentTextGradient = "bg-gradient-to-b from-[#FFD0A0] via-[#EED1B4] to-[#B2AF9E]"; 
+  const magicalStuffTextGradient = "bg-gradient-to-b from-[#212532] via-[#9CB5EA] to-[#3A3C45]"; 
+   
+  const dataTextStyle = `text-4xl text-[#EED1B4]`;
+
+  return (
+    
+    <div 
+      onClick={() => setItemModalShown(false)} 
+      className={`absolute flex justify-center items-center h-screen pb-32 w-full h-dhv top-0 left-0 bg-black/50 z-50 transition-opacity hover:cursor-pointer ${itemModalShown ? 'opacity-100' : 'opacity-0'} `}> 
+
+      <div 
+        onClick={(e) => e.stopPropagation()}
+        className='relative flex justify-center items-center hover:cursor-default'
+        style={{
+          backgroundImage: modalBackgroundImage,
+          backgroundPosition: "center",
+          backgroundSize: '100%',
+          width: 1024,
+          height: 683,
+        }}
+      >
+         
+        {/* Content Container */}
+        <div className="bg-red-500/0 w-3/4 h-3/5 py-3 flex flex-col gap-3">
+
+          {/* Row 1: Title */}
+          <div className="bg-blue-500/0 ">
+
+            {/* <h2 className="bg-yellow-700/10 text-center text-6xl py-2">{name}</h2> */}
+            <h2 className={`text-7xl py-2 bg-clip-text text-transparent select-text text-center ${isMagicalStuffShop(router) ? magicalStuffTextGradient : equipmentTextGradient}`}>
+              {name}
+            </h2>
+          
+          </div>
+
+          {/* Row 2: Itemdata, image, description */}
+          <div className="grid grid-cols-3 h-full bg-orange-300/0">
+
+            {/* Item Data */}
+            <div className="flex justify-center items-center flex-col ">  
+              <div>
+                {renderModifiers(modifiers, dataTextStyle)}
+                {(defense || base_percentage) && <hr className="w-5/6 mt-2 border-medievalSepia opacity-50 border-dashed"/> }
+                {defense && <p className={`${dataTextStyle}`}>{`Defense: ${defense}`}</p>}
+                {base_percentage && <p>{`Base Percentage: ${base_percentage}`}</p>}
+              </div>
+            </div>
+            
+            {/* Image */}
+            <div className="flex justify-center items-center flex-col gap-3">
+              <img  
+                className={`h-56 drop-shadow-[0_10px_10px_rgba(0,0,0,0.45)] ${isMagicalStuffShop(router) ? 'rounded-full border-3 border-[#1e1f23]' : null}`}
+                src={image_url}  
+                draggable={false}
+                onError={(e) => {
+                  e.currentTarget.onerror = null; // Prevent infinite loop if fallback also fails
+                  e.currentTarget.src = "/images/shop/buy/interrogation_sign.png"; // Fallback image
+                  e.currentTarget.title="Image not found"
+                }}
+              />
+              <div className={`grid place-items-center grid-cols-2 w-full mb-2`}>
+                <ItemDataLabel data={value} image={"/images/icons/gold.png"} title='Value' />
+                <ItemDataLabel data={min_lvl} image={"/images/icons/level.png"} title='Min. lvl' /> 
+              </div>
+            </div>
+
+            {/* Description */}
+            <div className="flex justify-center items-center bg">
+              <p
+                className={`text-4xl italic text-balance py-2 bg-clip-text text-transparent select-text text-center ${isMagicalStuffShop(router) ? magicalStuffTextGradient : equipmentTextGradient}`}
+              >
+                {`"${description}"`}
+              </p>
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+     
+  );
+
+} 
+
+// -------------------------- //
+// -----   BACKGROUND   ----- //
+// -------------------------- //
+
 const Background = () => {
   
   return (
@@ -500,7 +704,7 @@ const Background = () => {
 }
  
 // --------------------//
-// ----- UTILITY ----- //
+// ----  UTILITY  ---- //
 // ------------------- //
 
 const isEquipmentShop = (router): boolean => {
@@ -512,7 +716,30 @@ const isMagicalStuffShop = (router): boolean => {
   const currentShopType = router.query.shopType;
   return currentShopType === "magical_stuff";
 }
+ 
+// Capitalize first letter of string
+function capitalizeFirstLetter(string: string): string {
+  if (!string) return ''; // Handle empty string
+  return string.charAt(0).toUpperCase() + string.slice(1);
+} 
 
+const renderModifiers = (modifiers: Record<string, number>, textStyle: string) => {
+
+  if (!modifiers) {return}
+
+  return Object.entries(modifiers)
+    .map(([key, value]) => {
+      const attribute = key
+        ?.split('_')
+        .map((word) => capitalizeFirstLetter(word))
+        .join(' ');
+
+      if (value !== 0) {
+        return <p className={`${textStyle}`}>{`${attribute}: ${value}`}</p>;
+      }
+      return null;
+    });
+};
 
 export default Shop;  
 
