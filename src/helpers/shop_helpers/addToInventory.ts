4ff28db7295a76c95f0f2c6c8add136d62c52666
aitor <aitor.mendiburu@ikasle.aeg.eus>
2024-12-06 19:37:28 +0100

    [MENDI-547][UPDATE]refactory creating functions

---
+++ src/helpers/shop_helpers/addToInventory.ts
@@ -0,0 +1,26 @@
+export const addToInventory = (playerInventory: Record<string, any[]>, products: Array<{ _id: string, type: string }>) => {
+    products.forEach((product) => {
+      const category = product.type + 's'; // Determine the category based on the product type
+  
+      // Ensure the category exists in the player's inventory
+      if (!playerInventory[category]) {
+        playerInventory[category] = []; // Initialize the category if it doesn't exist
+      }
+  
+      // Check if the product is already in the specific category
+      const existingProduct = playerInventory[category].find(
+        (item) => item._id.toString() === product._id
+      );
+  
+      if (existingProduct) {
+        // If the product exists, increment the quantity
+        existingProduct.quantity = (existingProduct.quantity || 1) + 1;
+      } else {
+        // If the product does not exist, add it to the category
+        playerInventory[category].push({
+          ...product, // Add all product details
+          quantity: 1, // Initialize quantity
+        });
+      }
+    });
+  };
--- src/pages/api/shop/confirmPurchaseService.ts
+++ src/pages/api/shop/confirmPurchaseService.ts
@@ -1,5 +1,9 @@
 import { fetchPlayer, updatePlayer } from '../shop/playersService';
 import Player from '../../../database/models/playerModel';
+import { calculateTotalCost } from '@/helpers/shop_helpers/calculateTotalCost';
+import { hasEnoughGold } from '@/helpers/shop_helpers/hasEnoughGold';
+import { addToInventory } from '@/helpers/shop_helpers/addToInventory';
+
 
 export const processProductsPurchase = async (connection, playerEmail, products) => {
   if (!playerEmail || !Array.isArray(products) || products.length === 0) {
@@ -16,15 +20,10 @@ export const processProductsPurchase = async (connection, playerEmail, products)
   // console.log("los productos: " + JSON.stringify(products));
 
   // Calculate the total cost of all products
-  const totalCost = products.reduce((sum, product) => {
-    if (!product.value || !product._id) {
-      throw new Error('Each product must have a value and an _id');
-    }
-    return sum + product.value;
-  }, 0);
+  const totalCost = calculateTotalCost(products);
 
   // Check if the player has enough gold for all products
-  if (player.gold < totalCost) {
+  if (!hasEnoughGold(player.gold, totalCost)) {
     return {
       success: false,
       message: 'Insufficient funds',
@@ -35,30 +34,7 @@ export const processProductsPurchase = async (connection, playerEmail, products)
   player.gold -= totalCost;
 
   // Update the inventory with the purchased products
-  products.forEach((product) => {
-    const category = product.type + 's'; // Determine the category based on the product type
-
-    // Ensure the category exists in the player's inventory
-    if (!player.inventory[category]) {
-      player.inventory[category] = []; // Initialize the category if it doesn't exist
-    }
-
-    // Check if the product is already in the specific category
-    const existingProduct = player.inventory[category].find(
-      (item) => item._id.toString() === product._id
-    );
-
-    if (existingProduct) {
-      // If the product exists, increment the quantity
-      existingProduct.quantity = (existingProduct.quantity || 1) + 1;
-    } else {
-      // If the product does not exist, add it to the category
-      player.inventory[category].push({
-        ...product, // Add all product details
-        quantity: 1, // Initialize quantity
-      });
-    }
-  });
+  addToInventory(player.inventory, products);
 
   // Update the player document in the database
   const updatedPlayer = await updatePlayer(connection, playerEmail, {
