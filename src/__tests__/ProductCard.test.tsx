bba2c73c4d3e11876fc989bae05fa2fa626f74a7
ander <ander.zubizarreta@ikasle.aeg.eus>
2024-12-10 11:37:20 +0100

    [MENDI-602]Fix calculateTotalCost function to buy a product in equipment shop.

---
+++ src/__tests__/ProductCard.test.tsx
@@ -0,0 +1,12 @@
+import '@testing-library/jest-dom';
+import ItemCard from '../components/shop/buy/ItemCard';
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { Player } from "@/_common/interfaces/Player";
+import { CartItem } from '@/_common/interfaces/CartItem';
+
+
+describe("ProductCard Component", () => {
+
+
+});
--- src/helpers/shop_helpers/calculateTotalCost.ts
+++ src/helpers/shop_helpers/calculateTotalCost.ts
@@ -1,8 +1,13 @@
 export const calculateTotalCost = (products: Array<{ value: number, _id: string }>) => {
-    return products.reduce((sum, product) => {
-      if (!product.value || !product._id) {
-        throw new Error('Each product must have a value and an _id');
-      }
+  return products.reduce((sum, product) => {
+    if (!product.value || !product._id) {
+      throw new Error('Each product must have a value and an _id');
+    }
+    if (product.type === "ingredient") {
       return sum + product.value * product.quantity;
-    }, 0);
-  };
+    } else {
+      return sum + product.value;
+    }
+
+  }, 0);
+};
